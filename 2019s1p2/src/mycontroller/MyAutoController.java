091b8af48c8a9e38db1642f1db919f3e20c71663
yangxvlin <1768528843@qq.com>
2019-05-11 15:07:37 +1000

    fix indentation

--- 2019s1p2/src/mycontroller/MyAutoController.java
+++ 2019s1p2/src/mycontroller/MyAutoController.java
@@ -9,13 +9,13 @@ import utilities.Coordinate;
 import world.WorldSpatial;
 
 public class MyAutoController extends CarController{		
-		public MyAutoController(Car car) {
-			super(car);
-		}
-		
-		// Coordinate initialGuess;
-		// boolean notSouth = true;
-		@Override
-		public void update() {
-		}
+	public MyAutoController(Car car) {
+		super(car);
 	}
+
+	// Coordinate initialGuess;
+	// boolean notSouth = true;
+	@Override
+	public void update() {
+	}
+}
