086a34fc2c36ad46fb9dbaf0b6ed38b23f8e351b
yangxvlin <1768528843@qq.com>
2019-05-21 20:19:17 +1000

    update hard map

--- 2019s1p2/src/mycontroller/MyAutoController.java
+++ 2019s1p2/src/mycontroller/MyAutoController.java
@@ -2,19 +2,12 @@ package mycontroller;
 
 import controller.CarController;
 import world.Car;
-import java.util.HashMap;
-
-import tiles.MapTile;
-import utilities.Coordinate;
-import world.WorldSpatial;
 
 public class MyAutoController extends CarController{		
 	public MyAutoController(Car car) {
 		super(car);
 	}
 
-	// Coordinate initialGuess;
-	// boolean notSouth = true;
 	@Override
 	public void update() {
 	}
