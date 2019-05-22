31df9a3ad12f859560d0bd62b4b3460ff96c7a73
yangxvlin <1768528843@qq.com>
2019-05-22 16:26:10 +1000

    add a star

---
+++ 2019s1p2/src/mycontroller/GraphAlgorithm/AStar.java
@@ -0,0 +1,11 @@
+package mycontroller.GraphAlgorithm;
+
+/**
+ * Xulin Yang, 904904
+ *
+ * @create 2019-05-22 16:19
+ * description:
+ **/
+
+public class AStar {
+}
---
+++ 2019s1p2/src/mycontroller/GraphAlgorithm/State.java
@@ -0,0 +1,15 @@
+package mycontroller.GraphAlgorithm;
+
+/**
+ * Xulin Yang, 904904
+ *
+ * @create 2019-05-22 16:21
+ * description:
+ **/
+
+public class State {
+
+    public State() {
+
+    }
+}
